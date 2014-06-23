c123f5ff8f32cd9537ceffca79ec70d1af4ccb11
lvchao0428 <lvchao0428@163.com>
2014-06-22 21:00:43 -0400

    20140623

--- 20140622/bitree/src/main.c
+++ 20140622/bitree/src/main.c
@@ -2,8 +2,22 @@
 	> File Name: main.c
 	> Author: lvchao
 	> Mail:lvchao0428@163.com 
-	> Created Time: Sat 21 Jun 2014 11:11:19 PM EDT
+	> Created Time: Sun 22 Jun 2014 05:17:29 AM EDT
  ************************************************************************/
 
 #include<stdio.h>
+#include <malloc.h>
+#include "bitree.h"
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("enter a tree:\n");
+	pBiTree p = (pBiTree)malloc(sizeof(BiTree));
+	p = createBiTree();
+
+
+	preTraverse(p);
+	return 0;
+}
 
